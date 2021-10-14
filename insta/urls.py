96617403d717a51cc74600c71b20306d2784e3bf
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-14 19:28:03 +0300

    added login functionalities

--- insta/urls.py
+++ insta/urls.py
@@ -3,6 +3,11 @@ from .import views
 
 
 urlpatterns =[
+
+    path('register/',views.registerPage,name='register'),
+    path('login/',views.loginPage,name='login'),
+    path('logout/',views.userPage,name='user-page'),
+    path('user/',views.logoutUser,name='logout'),
     path('',views.logincup,name='auth'),
 
 
--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,63 @@
-from django.shortcuts import render
+from django.http import request
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+
+from .decorators import unauthenticated_user
+from .forms import CreateUserForm
+from django.contrib import messages
+from django .contrib.auth import authenticate, login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
+@unauthenticated_user
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account was created for ' + user)
+            return redirect('login')
+
+
+    context = {'form': form}
+    return render(request,'accounts/register.html',context)
+@unauthenticated_user
+def loginPage(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user = authenticate(request,username = username,password= password)
+        if user is not None:
+            login(request,user)
+            return redirect('auth')
+            
+        else:
+            messages.info(request,'Username or password is inorrect')
+            
+
+    context = {}
+    return render(request,'accounts/plogin.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+
+@login_required(login_url='login')
 def logincup(request):
-    return render(request,'index.html')
+    return render(request,'login.html')
+
+
+def userPage(request):
+    context = {}
+
+    return render(request,'accounts/user.html',context)
+
+
+
+
