d5b0cacf55695ed7f9fdac080d46d3a0ccc50290
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-15 00:24:52 +0300

    added profile class in models and migrated

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 
 
@@ -14,4 +14,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.author
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(default ='default.jpg',upload_to ='profile_pics')
+    
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
 
