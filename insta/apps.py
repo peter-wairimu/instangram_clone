0f3204461682caef1d56a6c30d7018eccf2b0fd2
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-16 14:19:26 +0300

    added signals file and installed signals

--- insta/apps.py
+++ insta/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class InstaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'insta'
+
+    def ready(self):
+        import signals
--- instagram/settings.py
+++ instagram/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'insta.apps.InstaConfig',
     'crispy_forms',
+    'signals',
 ]
 
 MIDDLEWARE = [
